26151c19128cae3e70e9cd38c193d7f05ec8f988
dschiffer12 <73139931+dschiffer12@users.noreply.github.com>
2023-01-11 10:16:51 -0700

    Toolbox

--- app.py
+++ app.py
@@ -1,105 +1,52 @@
-import time 
-import base64
 import streamlit as st
-import pandas as pd 
-import geopandas as gpd
-import numpy as np
-import matplotlib 
+from PIL import Image
 
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
+def remove_background(image):
+    # Code to remove background from image
+    return image
 
-import matplotlib.pyplot as plt
-import plotly_express as px 
+def get_primary_colors(image):
+    # Code to extract primary colors from image
+    return colors
 
-st.title("ViP Inc. Bulk Geocoding Application")
-st.markdown("Upload a CSV File with address columns (Street name & number, Postcode, City)")
+def add_watermark(image, watermark):
+    # Code to add watermark to image
+    return image
 
-
-
-def create_address_col(df):
-    st.sidebar.title("Select Address columns")
-    st.sidebar.info("You need to select address column (Street name and number), post code and City")
-    address_name = st.sidebar.selectbox("Select Address column", df.columns.tolist())
-    post_code = st.sidebar.selectbox("Select Post Code Column", df.columns.tolist())
-    city = st.sidebar.selectbox("Select the City Column", df.columns.tolist())
-    country = st.sidebar.text_input("Write the country of the addresses")
-    
-
-    df["geocode_col"] =  df[address_name].astype(str) + ',' + \
-                df[post_code] + ',' + \
-                df[city] + ',' + \
-                country   
-    return df
-    
-def choose_geocode_column(df):
-    selection = st.selectbox("Select the column", df.columns.tolist())
-    df["geocde_col"] = df[selection]
-    return df
-
-def geocode(df):
-    locator = Nominatim(user_agent="myGeocoder")
-    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
-    df['location'] = df['geocode_col'].apply(geocode)
-    df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
-
-    df[['latitude', 'longitude', 'altitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
-    return df 
-
-@st.cache(persist=True, suppress_st_warning=True)
-def display_map(df):
-    px.set_mapbox_access_token("pk.eyJ1Ijoic2hha2Fzb20iLCJhIjoiY2plMWg1NGFpMXZ5NjJxbjhlM2ttN3AwbiJ9.RtGYHmreKiyBfHuElgYq_w")
-    fig = px.scatter_mapbox(df, lat='latitude', lon='longitude', zoom=10)
-    return fig
-
-def download_csv(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-    return href
-
+def edit_and_crop(image):
+    # Code to allow editing and cropping of image
+    return image
 
 def main():
-    file = st.file_uploader("Choose a file")
+    st.set_page_config(page_title="Print on Demand Toolbox", page_icon=":guardsman:", layout="wide")
+    st.title("Print on Demand Toolbox")
+    
+    # File uploader
+    file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
     if file is not None:
-        file.seek(0)
-        df = pd.read_csv(file, low_memory=False)
-        with st.spinner('Reading CSV File...'):
-            time.sleep(5)
-            st.success('Done!')
-        st.write(df.head())
-        st.write(df.shape)
+        image = Image.open(file)
+        st.image(image, caption='Original Image', use_column_width=True)
 
+        # Remove background
+        if st.checkbox("Remove background"):
+            image = remove_background(image)
+            st.image(image, caption='Image with background removed', use_column_width=True)
 
+        # Extract primary colors
+        if st.checkbox("Extract primary colors"):
+            colors = get_primary_colors(image)
+            st.write("Primary colors:", colors)
 
-        cols = df.columns.tolist()
+        # Add watermark
+        if st.checkbox("Add watermark"):
+            watermark = st.text_input("Enter watermark text")
+            image = add_watermark(image, watermark)
+            st.image(image, caption='Image with watermark added', use_column_width=True)
 
-        st.subheader("Choose Address Columns from the Sidebar")
-        st.info("Example correct address: Karlaplan 13,115 20,STOCKHOLM, Sweden")
-    
-    if st.checkbox("Address Formatted correctly (Example Above)"):
-        df_address = choose_geocode_column(df)
-        st.write("choosing columns...")
-        st.write(df_address["geocode_col"].head())
-        st.write("Starting to Geocode")
-        geocoded_df = geocode(df_address)
-        with st.spinner('Geocoding Hold tight...'):
-            time.sleep(5)
-        st.success('Done!')
-        st.write(geocoded_df.head())
-        st.plotly_chart(display_map(geocoded_df))
-            
-        st.markdown(download_csv(geocoded_df), unsafe_allow_html=True)
-    
-    if st.checkbox("Not Correctly Formatted"):
-            df_address = create_address_col(df)
-            st.write(df_address["geocode_col"])
-            geocoded_df = geocode(df_address)
-            with st.spinner('Geocoding Hold tight...'):
-                time.sleep(5)
-            st.success('Done!')
-            st.plotly_chart(display_map(geocoded_df))
-            st.markdown(download_csv(geocoded_df), unsafe_allow_html=True)
+        # Edit and crop
+        if st.checkbox("Edit and crop"):
+            image = edit_and_crop(image)
+            st.image(image, caption='Edited and Cropped Image', use_column_width=True)
 
 if __name__ == "__main__":
     main()
